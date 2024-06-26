cba6207a97e4eff3a6a24c7e96420d46f5f7b249
venukumar <venubalijapelli14@gmail.com>
2024-06-26 19:53:54 +0530

    second commit

---
+++ src/test/java/com/example/kubernates_demo/KubernatesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kubernates_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernatesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
